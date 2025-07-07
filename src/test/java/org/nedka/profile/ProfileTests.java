c509f3e40e54761240e3192e8f0a5c0e01800445
pgnedka <pakovanedka@gmail.com>
2025-07-07 19:29:57 +0300

    Adding Test Automations - Profile Link Test

--- src/test/java/org/nedka/profile/ProfileTests.java
+++ src/test/java/org/nedka/profile/ProfileTests.java
@@ -1,15 +1,27 @@
 package org.nedka.profile;
 
-import org.nedka.POM.LoginPage;
+import org.nedka.POM.HomePage;
 import org.nedka.base.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProfileTests extends BaseTest {
 
-//    @Test
-//    public void verifyIfTheProfileLinkIsLoadedForNotLoggedInUser() {
-//        LoginPage loginPage = new LoginPage(super.driver,log);
-//        loginPage.
-//    }
+    @Test
+    public void verifyIfTheProfileLinkIsLoadedForNotLoggedInUser() throws InterruptedException {
 
+       log.info("STEP 1: Verify the user is on the Home page.");
+       HomePage homePage = new HomePage(super.driver, log);
+       homePage.navigateToHomePage();
+
+       log.info("STEP 2: Verify the user is not logged in.");
+       boolean isLogOutButtonPresented = homePage.isLogOutButtonShown();
+       Assert.assertFalse(isLogOutButtonPresented);
+
+       log.info("STEP 3: Check if the Profile link is visible on the screen.");
+       Assert.assertFalse(homePage.isNavBarProfileLinkShown());
+
+       Thread.sleep(3000);
+       System.out.println("The end!");
+    }
 }
